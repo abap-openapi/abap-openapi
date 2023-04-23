6f23da62cf58bfc597b268de1cbc4995d1e69212
Lars Hvam <larshp@hotmail.com>
2023-04-23 11:26:48 +0200

    fix

--- test_v2/test.mjs
+++ test_v2/test.mjs
@@ -29,10 +29,10 @@ async function run() {
     );
     const result = await abap.Classes["ZCL_OAPI_GENERATOR"].generate_v2({is_input: input});
 
-    fs.writeFileSync(folder + input.get().clas_icf_serv.get() + ".clas.abap", result.get().clas_icf_serv.get());
-    fs.writeFileSync(folder + input.get().clas_icf_impl.get() + ".clas.abap", result.get().clas_icf_impl.get());
-    fs.writeFileSync(folder + input.get().clas_client.get() + ".clas.abap", result.get().clas_client.get());
-    fs.writeFileSync(folder + input.get().intf.get() + ".intf.abap", result.get().intf.get());
+    fs.writeFileSync(folder + input.get().clas_icf_serv.get().trimEnd() + ".clas.abap", result.get().clas_icf_serv.get());
+    fs.writeFileSync(folder + input.get().clas_icf_impl.get().trimEnd() + ".clas.abap", result.get().clas_icf_impl.get());
+    fs.writeFileSync(folder + input.get().clas_client.get().trimEnd() + ".clas.abap", result.get().clas_client.get());
+    fs.writeFileSync(folder + input.get().intf.get().trimEnd() + ".intf.abap", result.get().intf.get());
 
     const consoleOutput = abap.console.get();
     if (consoleOutput !== "") {
